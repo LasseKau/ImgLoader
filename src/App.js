04ef4ccc04c47214a2ea405ca71c5121d0733251
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-08 12:14:04 +0300

    postprocessing
    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useRef, Suspense, useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+//import './styles.css'
+import { Canvas, useFrame } from 'react-three-fiber'
+import { EffectComposer, Noise, Glitch } from 'react-postprocessing'
+import { BlendFunction } from 'postprocessing'
+
 
 import UploadImages from "./components/image-upload.component";
 
@@ -11,9 +16,19 @@ function App() {
       <h4>Add Image</h4>
 
       <div className="content">
+      <Canvas>
+
+          <Suspense fallback={null}>
+            <EffectComposer>
+              <Glitch />
+              <Noise blendFunction={BlendFunction.MULTIPLY} />
+            </EffectComposer>
+          </Suspense>
+        </Canvas>
         <UploadImages />
       </div>
     </div>
+    
   );
 }
 
