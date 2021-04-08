c7d4e2ff32ada1f31fde58cba8b1f3ccd4428625
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-08 10:13:29 +0300

    update
    -

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import UploadImages from "./components/images-upload.component";
+import UploadImages from "./components/image-upload.component";
 
 function App() {
   return (
     <div className="container">
       <h3>bezkoder.com</h3>
-      <h4>React Multiple Images Upload with Preview</h4>
+      <h4>React Image Upload with Preview</h4>
 
       <div className="content">
         <UploadImages />
